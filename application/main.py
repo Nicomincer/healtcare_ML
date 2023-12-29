7aa945eb3447f01f92ca5584e2511cc7cdcfe48c
Vesper <nicolasalencar100@gmail.com>
2023-12-29 10:20:41 -0300

    normalização de dados

--- application/main.py
+++ application/main.py
@@ -4,15 +4,29 @@ import pandas as pd
 import numpy as np 
 from pathlib import Path 
 
-caminho = Path(Path.home(), "Documentos/healtcare_ML")
-dataset = pd.read_csv(f"{caminho}/healthcare_dataset.csv").drop(["Room Number", "Discharge Date", "Admission Type", "Date of Admission"], axis=1)
+caminho = Path(Path.home(), "Documentos/healthcare_ML/")
+dataset = pd.read_csv(f"{caminho}/dataset_healthcare/healthcare_dataset.csv").drop(["Room Number", "Discharge Date", "Admission Type", "Date of Admission", "Name", "Hospital", "Doctor"], axis=1)
 dataset['Gender'] = (dataset["Gender"] == "Female").astype(int)
-
-for valor in range(0, int(dataset["Blood Type"].unique())):
-    dataset["Gender"] 
-
 train, valid, test = np.split(dataset.sample(frac=1), [int(0.6*len(dataset)), int(0.8*len(dataset))])
+X_train = dataset[dataset.columns[0:-2]].values
+y_train = dataset[dataset.columns[-1]].values
+y_test = dataset[dataset.columns[-1]].values
+tipos_sanguineos = list(dataset["Blood Type"].unique())
+label_sangue = {"O-": 0, "O+": 1, "B-": 2, "AB+": 3, "A+":4, "AB-": 5, "A-": 6, "B+": 7}
+label_medical = {'Diabetes': 0,  'Asthma': 1,  'Obesity': 2, 'Arthritis': 3, 'Hypertension':4, 'Cancer': 5}
+label_insurance = {'Medicare': 0, 'UnitedHealthcare': 1, 'Aetna': 2, 'Cigna': 3, 'Blue Cross': 4}
+label_medication = {'Aspirin': 0, 'Lipitor': 1, 'Penicillin': 2, 'Paracetamol': 3, 'Ibuprofen': 4}
+label_results = {'Inconclusive': 0,  'Normal': 1, 'Abnormal': 2}
 
-print(dataset["Blood Type"].value_counts())
+dataset["Test Results"] = dataset["Test Results"].replace(label_results)
+dataset["Medication"] = dataset["Medication"].replace(label_medication)
+dataset["Insurance Provider"] = dataset["Insurance Provider"].replace(label_insurance)
+dataset["Medical Condition"] = dataset["Medical Condition"].replace(label_medical)
+dataset["Blood Type"] = dataset["Blood Type"].replace(label_sangue)
+
+print(dataset.info())
+
+
+
 
 
